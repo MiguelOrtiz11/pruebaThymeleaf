90d60be76dedfa64a74c119be48cf7b192d929e7
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-04-29 00:17:53 -0500

    Creación de una lista de Personas

--- src/main/java/com/example/prueba/controlador.java
+++ src/main/java/com/example/prueba/controlador.java
@@ -7,36 +7,54 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @Slf4j
 
 public class controlador {
 
-	@Value("${index.saludo}")
-	private String saludo;
+    @Value("${index.saludo}")
+    private String saludo;
 
-	@GetMapping("/")
-	public String inicio(Model model) {
-		
-		
-	var mensaje = "Mensaje enviado desde el controlador";
-		//log.debug("Ejecuando controlador Spring MVC");
+    @GetMapping("/")
+    public String inicio(Model model) {
 
 
-		// Creamos un objeto de la clase persona
-		var persona =  new Persona();
-		persona.setNombre("Juan");
-		persona.setApellido("Perez");
-		persona.setEmail("juanperez@gmail.com");
-		persona.setTelefono("1232133");
+        var mensaje = "Mensaje enviado desde el controlador";
+        //log.debug("Ejecuando controlador Spring MVC");
 
-		//Llamamos el metodo persona, hacia el index
-		model.addAttribute("persona", persona);
-		model.addAttribute("mensaje", mensaje);
-		model.addAttribute("saludo", saludo);
 
-		//Nombre de la pagina que se visualiza en el navegador
-		return "index";
-	}
+        // Creamos un objeto de la clase persona
+        var persona1 = new Persona();
+        persona1.setNombre("Juan");
+        persona1.setApellido("Perez");
+        persona1.setEmail("juanperez@gmail.com");
+        persona1.setTelefono("1232133");
+
+        // Creamos un objeto de la clase persona
+        var persona2 = new Persona();
+        persona2.setNombre("Karla");
+        persona2.setApellido("Gomez");
+        persona2.setEmail("karlagomer@gmail.com");
+        persona2.setTelefono("42345463");
+
+        //Array list de objetos tipo personas
+        //	var personas = new Arraylist();
+        //	personas.add(personas);
+        //	personas.add(personas);
+
+        var personas = Arrays.asList(persona1, persona2);
+
+        //Llamamos el metodo persona, hacia el index
+        model.addAttribute("personas", personas);
+//		model.addAttribute("persona", persona1);
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
+
+        //Nombre de la pagina que se visualiza en el navegador
+        return "index";
+    }
 
 }
